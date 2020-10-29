5c77a621f1f57b9747169ef5552792490ba4aeeb
Ashwin Kumar Rajagopalan <ash23win@Ashwins-MacBook-Air.local>
2020-10-29 19:13:53 +0000

    Add 3 sensor sorbent and improve plotting

--- plotFunctions/plotConcentrationEstimate.py
+++ plotFunctions/plotConcentrationEstimate.py
@@ -12,6 +12,7 @@
 # Plots to visualize estimated concentration
 #
 # Last modified:
+# - 2020-10-29, AK: Improvements to the plots
 # - 2020-10-23, AK: Initial creation
 #
 # Input arguments:
@@ -27,7 +28,7 @@ from numpy import load
 import os
 import auxiliaryFunctions
 import matplotlib.pyplot as plt
-plt.style.use('doubleColumn.mplstyle') # Custom matplotlib style file
+plt.style.use('singleColumn.mplstyle') # Custom matplotlib style file
 
 # Flag for saving figure
 saveFlag = False
@@ -35,68 +36,41 @@ saveFlag = False
 # Save file extension (png or pdf)
 saveFileExtension = ".png"
 
+# Plot zoomed plot
+plotZoomDist = False
+
+# Gas concentration
+molFracG1 = 0.05
+molFracG2 = 0.15
+molFracG3 = 1 - molFracG1 - molFracG2
+
 # Xlimits and Ylimits
 xLimits = [0,1]
+yLimits = [0,100] 
 xLimitsSum = [0,2]
-yLimits = [0,40]
+yLimitsSum = [0,80]
 
-# Number of bins for histogram
-nBins = 10 
+xLimitsZ1 = [0,0.1] # Limits for zoom gas 1
+xLimitsZ2 = [0.05,0.25] # Limits for zoom gas 2
+xLimitsZ3 = [0.75,0.85] # Limits for zoom gas 3
+yLimitsZ1 = [0,5] # Limits for zoom gas 1
+yLimitsZ2 = [0,5] # Limits for zoom gas 2
+yLimitsZ3 = [0,5] # Limits for zoom gas 3
+xLimitsSumZS = [0.9,1.1] # Limits for zoom gas sum
+yLimitsSumZS = [0,5] # Limits for zoom gas sum
+
+# Histogram properties
+nBins = 100
+rangeX = (xLimits)
+rangeXS = (xLimitsSum)
+histTypeX = 'stepfilled'
+alphaX=0.5
+densityX = False
 
 # For now load a given adsorbent isotherm material file
-# loadFileName = "arrayConcentration_20201026_1239_6bf5c35.npy" # Three gases
 loadFileName = "arrayConcentration_20201026_1240_6bf5c35.npy" # Two sensors
+# loadFileName = "arrayConcentration_20201029_1227_a726870.npy" # Three sensors
 simResultsFile = os.path.join('..','simulationResults',loadFileName);
-
-# Check if the file with the adsorbent properties exist 
-if os.path.exists(simResultsFile):
-    resultOutput = load(simResultsFile)
-    if resultOutput.shape[1] == 4:
-        resultOutput = np.delete(resultOutput,0,1)
-    elif resultOutput.shape[1] == 5:
-        resultOutput = np.delete(resultOutput,[0,1],1)
-else:
-    errorString = "Simulation result file " + simResultsFile + " does not exist."
-    raise Exception(errorString)
-
-# Plot the pure single component isotherm for the n gases
-fig = plt.figure
-# Histogram for gas 1
-ax1 = plt.subplot(1,4,1)
-ax1.hist(resultOutput[:,0], bins = nBins,
-         linewidth=1.5, histtype = 'step', color='r', label = '$g_1$')
-ax1.set(xlabel='$y_1$ [-]', 
-        ylabel='$f$ [-]',
-        xlim = xLimits, ylim = yLimits)
-ax1.locator_params(axis="x", nbins=4)
-ax1.locator_params(axis="y", nbins=4)
-
-# Histogram for gas 2
-ax2 = plt.subplot(1,4,2)
-ax2.hist(resultOutput[:,1], bins = nBins,
-         linewidth=1.5, histtype = 'step', color='b', label = '$g_2$')
-ax2.set(xlabel='$y_2$ [-]', 
-        xlim = xLimits, ylim = yLimits)
-ax2.locator_params(axis="x", nbins=4)
-ax2.locator_params(axis="y", nbins=4)
-
-# Histogram for gas 3
-ax3 = plt.subplot(1,4,3)
-ax3.hist(resultOutput[:,2], bins = nBins,
-         linewidth=1.5, histtype = 'step', color='g', label = '$g_3$')
-ax3.set(xlabel='$y_3$ [-]', 
-        xlim = xLimits, ylim = yLimits)
-ax3.locator_params(axis="x", nbins=4)
-ax3.locator_params(axis="y", nbins=4)
-
-# Histogram for the sum of mole fraction
-ax3 = plt.subplot(1,4,4)
-ax3.hist(np.sum(resultOutput,1), bins = nBins,
-         linewidth=1.5, histtype = 'step', color='k')
-ax3.set(xlabel='$\Sigma y$ [-]', 
-        xlim = xLimitsSum, ylim = yLimits)
-ax3.locator_params(axis="x", nbins=4)
-ax3.locator_params(axis="y", nbins=4)
 
 # Get the commit ID of the current repository
 gitCommitID = auxiliaryFunctions.getCommitID()
@@ -107,10 +81,75 @@ currentDT = auxiliaryFunctions.getCurrentDateTime()
 # Git commit id of the loaded isotherm file
 simID_loadedFile = loadFileName[-21:-4]
 
+# Check if the file with the adsorbent properties exist 
+if os.path.exists(simResultsFile):
+    resultOutput = load(simResultsFile)
+    if resultOutput.shape[1] == 4:
+        resultOutput = np.delete(resultOutput,0,1)
+    elif resultOutput.shape[1] == 5:
+        resultOutput = np.delete(resultOutput,[0,1],1)
+    elif resultOutput.shape[1] == 6:
+        resultOutput = np.delete(resultOutput,[0,1,2],1)
+else:
+    errorString = "Simulation result file " + simResultsFile + " does not exist."
+    raise Exception(errorString)
+
+# Plot the pure single component isotherm for the n gases
+fig = plt.figure
+# Histogram for gas 1
+ax1 = plt.subplot(1,1,1)
+ax1.hist(resultOutput[:,0], bins = nBins, range = rangeX, density = densityX, 
+         linewidth=1.5, histtype = histTypeX, color='r', alpha = alphaX, label = '$g_1$')
+ax1.axvline(x=molFracG1, linewidth=1, linestyle='dotted', color = 'r', alpha = 0.6)
+
+# Histogram for gas 2
+# ax2 = plt.subplot(1,4,2)
+ax1.hist(resultOutput[:,1], bins = nBins, range = rangeX, density = densityX, 
+         linewidth=1.5, histtype = histTypeX, color='b', alpha = alphaX, label = '$g_2$')
+ax1.axvline(x=molFracG2, linewidth=1, linestyle='dotted', color = 'b', alpha = 0.6)
+
+# Histogram for gas 3
+# ax3 = plt.subplot(1,4,3)
+ax1.hist(resultOutput[:,2], bins = nBins, range = rangeX, density = densityX, 
+         linewidth=1.5, histtype = histTypeX, color='g', alpha = alphaX, label = '$g_3$')
+ax1.axvline(x=molFracG3, linewidth=1, linestyle='dotted', color = 'g', alpha = 0.6)
+
+ax1.locator_params(axis="x", nbins=4)
+ax1.locator_params(axis="y", nbins=4)
+ax1.set(xlabel='$y_i$ [-]', 
+        ylabel='$f$ [-]',
+        xlim = xLimits, ylim = yLimits)
+ax1.legend()
+
 #  Save the figure
 if saveFlag:
     # FileName: ConcEstimate_<currentDateTime>_<GitCommitID_Current>_<MMdd_GitCommitID_Data>
     saveFileName = "ConcEstimate_" + currentDT + "_" + gitCommitID + "_" + simID_loadedFile + saveFileExtension
+    savePath = os.path.join('..','simulationFigures',saveFileName)
+    # Check if inputResources directory exists or not. If not, create the folder
+    if not os.path.exists(os.path.join('..','simulationFigures')):
+        os.mkdir(os.path.join('..','simulationFigures'))
+    plt.savefig (savePath)
+   
+    
+# For the figure to be saved show should appear after the save
+plt.show()
+
+# Histogram for the sum of mole fraction
+fig = plt.figure
+ax2 = plt.subplot(1,1,1)
+ax2.hist(np.sum(resultOutput,1), bins = nBins, range = rangeXS, density = densityX, 
+         linewidth=1.5, histtype = histTypeX, color='k',  alpha = alphaX)
+ax2.axvline(x=1., linewidth=1, linestyle='dotted', color = 'k')
+ax2.set(xlabel='$\Sigma y_i$ [-]', 
+        xlim = xLimitsSum, ylim = yLimitsSum)
+ax2.locator_params(axis="x", nbins=4)
+ax2.locator_params(axis="y", nbins=4)
+
+#  Save the figure
+if saveFlag:
+    # FileName: ConcEstimate_<currentDateTime>_<GitCommitID_Current>_<MMdd_GitCommitID_Data>
+    saveFileName = "ConcEstimateSum_" + currentDT + "_" + gitCommitID + "_" + simID_loadedFile + saveFileExtension
     savePath = os.path.join('..','simulationFigures',saveFileName)
     # Check if inputResources directory exists or not. If not, create the folder
     if not os.path.exists(os.path.join('..','simulationFigures')):
--- screenSensorArray.py
+++ screenSensorArray.py
@@ -13,6 +13,7 @@
 # sorbents are assumed to exhibit Langmuirian behavior.
 #
 # Last modified:
+# - 2020-10-29, AK: Add 3 sorbent sensor
 # - 2020-10-28, AK: Add auxiliary functions as a module
 # - 2020-10-22, AK: Initial creation
 #
@@ -33,6 +34,9 @@ from tqdm import tqdm # To track progress of the loop
 from estimateConcentration import estimateConcentration
 import os
 
+# Number of sensors in the array
+numSensors = 3
+
 # Get the commit ID of the current repository
 gitCommitID = auxiliaryFunctions.getCommitID()
 
@@ -48,64 +52,102 @@ numberOfGases = 3
 # Check generateTrueSensorResponse.py for the actual concentrations
 moleFracID = 0
 
-##### FOR 1 SORBENT SENSOR ARRAY #####
-# Get the current date and time for saving purposes    
-simulationDT = auxiliaryFunctions.getCurrentDateTime()
-
-# Loop over all the sorbents for a single material sensor
-# Using parallel processing to loop through all the materials
-arrayConcentration = np.zeros(numberOfAdsorbents)
-arrayConcentration = Parallel(n_jobs=num_cores, prefer="threads")(delayed(estimateConcentration)
-                                                                  (numberOfAdsorbents,numberOfGases,moleFracID,[ii])
-                                                                  for ii in tqdm(range(numberOfAdsorbents)))
-
-# Convert the output list to a matrix
-arrayConcentration = np.array(arrayConcentration)
-
-# Save the array concentration into a native numpy file
-# The .npy file is saved in a folder called simulationResults (hardcoded)
-filePrefix = "arrayConcentration"
-saveFileName = filePrefix + "_" + simulationDT + "_" + gitCommitID;
-savePath = os.path.join('simulationResults',saveFileName)
-
-# Check if inputResources directory exists or not. If not, create the folder
-if not os.path.exists('simulationResults'):
-    os.mkdir('simulationResults')
-
-# Save the array ceoncentration obtained from estimateConcentration
-save (savePath, arrayConcentration)
-
-##### FOR 2 SORBENT SENSOR ARRAY #####
-# Get the current date and time for saving purposes    
-simulationDT = auxiliaryFunctions.getCurrentDateTime()
-
-# Loop over all the sorbents for a single material sensor
-# Using parallel processing to loop through all the materials
-arrayConcentration = np.zeros(numberOfAdsorbents)
-for jj in range(numberOfAdsorbents):
-    arrayConcentrationTemp = Parallel(n_jobs=num_cores, prefer="threads")(delayed(estimateConcentration)
-                                                                  (numberOfAdsorbents,numberOfGases,moleFracID,[ii,jj])
-                                                                  for ii in tqdm(range(jj,numberOfAdsorbents)))
-    # Convert the output list to a matrix
-    arrayConcentrationTemp = np.array(arrayConcentrationTemp)
-    if jj == 0:
-        arrayConcentration = arrayConcentrationTemp
-    else:
-        arrayConcentration = np.append(arrayConcentration,arrayConcentrationTemp, axis=0)
+# Check for number of sensors in the array
+if numSensors == 1:
+    ##### FOR 1 SORBENT SENSOR ARRAY #####
+    # Get the current date and time for saving purposes    
+    simulationDT = auxiliaryFunctions.getCurrentDateTime()
     
-# Delete entries that use the same materials for both sensors
-delRows = np.where(arrayConcentration[:,0] == arrayConcentration[:,1])
-arrayConcentration = np.delete(arrayConcentration,delRows,axis=0)
+    # Loop over all the sorbents for a single material sensor
+    # Using parallel processing to loop through all the materials
+    arrayConcentration = np.zeros(numberOfAdsorbents)
+    arrayConcentration = Parallel(n_jobs=num_cores, prefer="threads")(delayed(estimateConcentration)
+                                                                      (numberOfAdsorbents,numberOfGases,moleFracID,[ii])
+                                                                      for ii in tqdm(range(numberOfAdsorbents)))
+    
+    # Convert the output list to a matrix
+    arrayConcentration = np.array(arrayConcentration)
+    
+    # Save the array concentration into a native numpy file
+    # The .npy file is saved in a folder called simulationResults (hardcoded)
+    filePrefix = "arrayConcentration"
+    saveFileName = filePrefix + "_" + simulationDT + "_" + gitCommitID;
+    savePath = os.path.join('simulationResults',saveFileName)
+    
+    # Check if inputResources directory exists or not. If not, create the folder
+    if not os.path.exists('simulationResults'):
+        os.mkdir('simulationResults')
+    
+    # Save the array ceoncentration obtained from estimateConcentration
+    save (savePath, arrayConcentration)
 
-# Save the array concentration into a native numpy file
-# The .npy file is saved in a folder called simulationResults (hardcoded)
-filePrefix = "arrayConcentration"
-saveFileName = filePrefix + "_" + simulationDT + "_" + gitCommitID;
-savePath = os.path.join('simulationResults',saveFileName)
+elif numSensors == 2:
+    ##### FOR 2 SORBENT SENSOR ARRAY #####
+    # Get the current date and time for saving purposes    
+    simulationDT = auxiliaryFunctions.getCurrentDateTime()
+    
+    # Loop over all the sorbents for a single material sensor
+    # Using parallel processing to loop through all the materials
+    arrayConcentration = np.zeros(numberOfAdsorbents)
+    for jj in range(numberOfAdsorbents-1):
+        arrayConcentrationTemp = Parallel(n_jobs=num_cores, prefer="threads")(delayed(estimateConcentration)
+                                                                      (numberOfAdsorbents,numberOfGases,moleFracID,[ii,jj])
+                                                                      for ii in tqdm(range(jj+1,numberOfAdsorbents)))
+        # Convert the output list to a matrix
+        arrayConcentrationTemp = np.array(arrayConcentrationTemp)
+        if jj == 0:
+            arrayConcentration = arrayConcentrationTemp
+        else:
+            arrayConcentration = np.append(arrayConcentration,arrayConcentrationTemp, axis=0)
+    
+    # Save the array concentration into a native numpy file
+    # The .npy file is saved in a folder called simulationResults (hardcoded)
+    filePrefix = "arrayConcentration"
+    saveFileName = filePrefix + "_" + simulationDT + "_" + gitCommitID;
+    savePath = os.path.join('simulationResults',saveFileName)
+    
+    # Check if inputResources directory exists or not. If not, create the folder
+    if not os.path.exists('simulationResults'):
+        os.mkdir('simulationResults')
+    
+    # Save the array ceoncentration obtained from estimateConcentration
+    save (savePath, arrayConcentration)
 
-# Check if inputResources directory exists or not. If not, create the folder
-if not os.path.exists('simulationResults'):
-    os.mkdir('simulationResults')
+elif numSensors == 3:
+    ##### FOR 3 SORBENT SENSOR ARRAY #####
+    # Get the current date and time for saving purposes    
+    simulationDT = auxiliaryFunctions.getCurrentDateTime()
+    
+    # Loop over all the sorbents for a single material sensor
+    # Using parallel processing to loop through all the materials
+    arrayConcentration = np.zeros(numberOfAdsorbents)
 
-# Save the array ceoncentration obtained from estimateConcentration
-save (savePath, arrayConcentration)
+    for kk in range(numberOfAdsorbents-1):    
+        for jj in range(kk+1,numberOfAdsorbents-1):
+            arrayConcentrationTemp = Parallel(n_jobs=num_cores, prefer="threads")(delayed(estimateConcentration)
+                                                                          (numberOfAdsorbents,numberOfGases,moleFracID,[ii,jj,kk])
+                                                                          for ii in tqdm(range(jj+1,numberOfAdsorbents)))
+            # Convert the output list to a matrix
+            arrayConcentrationTemp = np.array(arrayConcentrationTemp)
+            if kk == 0 and jj == 1:
+                arrayConcentration = arrayConcentrationTemp
+            else:
+                arrayConcentration = np.append(arrayConcentration,arrayConcentrationTemp, axis=0)
+        
+    # Delete entries that use the same materials for all three sensors
+    # delRows = np.where(np.logical_and(arrayConcentration[:,0] == arrayConcentration[:,1],
+    #                                   arrayConcentration[:,0] == arrayConcentration[:,2]))
+    # arrayConcentration = np.delete(arrayConcentration,delRows,axis=0)
+    
+    # Save the array concentration into a native numpy file
+    # The .npy file is saved in a folder called simulationResults (hardcoded)
+    filePrefix = "arrayConcentration"
+    saveFileName = filePrefix + "_" + simulationDT + "_" + gitCommitID;
+    savePath = os.path.join('simulationResults',saveFileName)
+    
+    # Check if inputResources directory exists or not. If not, create the folder
+    if not os.path.exists('simulationResults'):
+        os.mkdir('simulationResults')
+    
+    # Save the array ceoncentration obtained from estimateConcentration
+    save (savePath, arrayConcentration)
