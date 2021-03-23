c067fe72974f6f3cad6bccaffc927b6407c3b1ba
Ashwin Kumar Rajagopalan <ash23win@dyn3219-60.wlan.ic.ac.uk>
2021-03-23 12:12:51 +0000

    Minor fixes for full model wrapper

--- sensorFullModelWrapper.py
+++ sensorFullModelWrapper.py
@@ -13,6 +13,7 @@
 # variables
 #
 # Last modified:
+# - 2021-03-23, AK: Minor fixes
 # - 2021-02-03, AK: Change output plot response to absolute values
 # - 2021-01-20, AK: Initial creation
 #
@@ -29,10 +30,12 @@ from simulateFullModel import simulateFullModel
 from tqdm import tqdm # To track progress of the loop
 import numpy as np
 import os
+from numpy import savez
+import socket
 import matplotlib.pyplot as plt
 
-# Save settings
-saveFlag = False
+# Save and plot settings
+saveFlag = True
 saveFileExtension = ".png"
 saveText = "rateConstant"
 colorsForPlot = ["1DBDE6","52A2C4","6D95B3","A27A91","CA6678","F1515E"]
@@ -46,7 +49,7 @@ currentDT = auxiliaryFunctions.getCurrentDateTime()
 # Define the variable to be looped
 # This has to be a be a tuple. For on condition write the values followed by a 
 # comma to say its a tuple
-volTotal = 5e-7
+volTotal = 10e-7
 loopVariable = (0.001,0.25,0.5,0.75,0.9)
     
 # Define a dictionary
@@ -61,6 +64,16 @@ for ii in tqdm(range(len(loopVariable))):
                 'sensorFingerPrint':sensorFingerPrint,
                 'inputParameters':inputParameters}
     
+# Save the array concentration into a native numpy file
+# The .npz file is saved in a folder called simulationResults (hardcoded)
+#  Save the figure
+if saveFlag:
+    filePrefix = "fullModelSensitivity"
+    saveFileName = filePrefix + "_" + currentDT + "_" + gitCommitID;
+    savePath = os.path.join('simulationResults',saveFileName)
+    savez (savePath, outputStruct = outputStruct, # True response
+            hostName = socket.gethostname()) # Hostname of the computer    
+
 # Plot the sensor finger print
 os.chdir("plotFunctions")
 plt.style.use('singleColumn.mplstyle') # Custom matplotlib style file
@@ -79,14 +92,5 @@ ax.set(xlabel='$t$ [s]',
    xlim = [timeSim[0], timeSim[-1]], ylim = [0, None])
 ax.locator_params(axis="x", nbins=4)
 ax.locator_params(axis="y", nbins=4)
-#  Save the figure
-if saveFlag:
-    # FileName: fullModelWrapper_<saveText>_<currentDateTime>_<gitCommitID>
-    saveFileName = "fullModelWrapper_" + saveText + "_" + currentDT + "_" + gitCommitID + saveFileExtension
-    savePath = os.path.join('..','simulationFigures',saveFileName.replace('[','').replace(']',''))
-    # Check if inputResources directory exists or not. If not, create the folder
-    if not os.path.exists(os.path.join('..','simulationFigures')):
-        os.mkdir(os.path.join('..','simulationFigures'))
-    plt.savefig (savePath)
 plt.show()
 os.chdir("..")
