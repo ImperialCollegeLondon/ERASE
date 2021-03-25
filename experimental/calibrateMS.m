a713a518c2b011a22319de0a2813b6681cca7ff7
Ashwin <15141279+ash23win@users.noreply.github.com>
2021-03-25 18:22:54 +0000

    Small fix to perform multiple calibration

--- experimental/calibrateMS.m
+++ experimental/calibrateMS.m
@@ -23,8 +23,6 @@ function calibrateMS
     % Experiment name
     expInfo.expName = ['ZLCCalibrateMS','_',...
         datestr(datetime('now'),'yyyymmdd')];
-    % Maximum time of the experiment
-    expInfo.maxTime = 300;
     % Sampling time for the device
     expInfo.samplingTime = 2;
     % Define gas for MFM
@@ -34,7 +32,7 @@ function calibrateMS
     % Define gas for MFC2
     expInfo.gasName_MFC2 = 'CO2';
     % Define set point for MFC1
-    MFC1_SP = [0.2, 1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5 14.8];
+    MFC1_SP = repmat([0.0 0.2 1.5 3.0 4.5, 6.0 7.5, 9.0 10.5 12.0 13.5 14.8 15.0],[1,2]);
     % Define set point for MFC2
     MFC2_SP = 15.0-MFC1_SP;
     % Loop through all setpoints to calibrate the meters
@@ -43,6 +41,16 @@ function calibrateMS
         expInfo.MFC2_SP = MFC2_SP(ii);
         % Flag for meter calibration
         expInfo.calibrateMeters = true;
+        % When the set point goes back to zero wait for 5 more min before
+        % starting the measurement
+        if ii == find(MFC1_SP == 0,1,'last')
+            % Maximum time of the experiment
+            % Change the max time to 10 min
+            expInfo.maxTime = 600;
+        else
+            % Else use 5 min
+            expInfo.maxTime = 300;
+        end
         % Run the setup for different calibrations
         runZLC(expInfo)
     end
