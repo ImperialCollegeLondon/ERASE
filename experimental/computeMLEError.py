Fix error computation for parameter estimation (major)

experimental/computeMLEError.py
@@ -12,6 +12,7 @@
 # Computes the MLE error for ZLC experiments.
 #
 # Last modified:
+# - 2021-06-12, AK: Add pure data downsampling
 # - 2021-05-24, AK: Add -inf input to avoid splitting compositions
 # - 2021-05-13, AK: Add different modes for MLE error computations
 # - 2021-05-05, AK: Initial creation
@@ -27,52 +28,101 @@
 def computeMLEError(moleFracExp,moleFracSim,**kwargs):
     import numpy as np
 
-    # Check if threshold is provided to split data to high and low compositions    
-    if 'thresholdFactor' in kwargs:
-        thresholdFlag = True
-        thresholdFactor = np.array(kwargs["thresholdFactor"])
-        # If negative infinity provided as a threshold, do not split and uses 
-        # all data with equal weights
-        if np.isneginf(thresholdFactor):
-            thresholdFlag = False            
-    # Default is flag, uses all data with equal weights
+    # Check if flag for downsampling data in low and high composition provided
+    if 'downsampleData' in kwargs:
+        downsampleData = kwargs["downsampleData"]
+        # Threshold Factor is needed for pure downsampling
+        if 'thresholdFactor' in kwargs:
+            thresholdFactor = np.array(kwargs["thresholdFactor"])
+        else:
+            thresholdFactor = 0.5 # Default set to 0.5 (as data scaled between 0 and 1)
+    # Default is false, uses either pure MLE or split MLE with scaling based 
+    # on threshold
     else:
-        thresholdFlag = False
+        downsampleData = False
+        # Check if threshold is provided to split data to high and low 
+        # compositions and scale for MLE computation    
+        if 'thresholdFactor' in kwargs:
+            thresholdFlag = True
+            thresholdFactor = np.array(kwargs["thresholdFactor"])
+            # If negative infinity provided as a threshold, do not split and uses 
+            # all data with equal weights
+            if np.isneginf(thresholdFactor):
+                thresholdFlag = False            
+        # Default is false, uses all data with equal weights
+        else:
+            thresholdFlag = False
 
-    # If no threshold provided just do normal MLE
-    if not thresholdFlag:    
-        computedError = np.log(np.sum(np.power(moleFracExp - moleFracSim, 2)))
-        numPoints = len(moleFracExp)
-    # If threshold provided, split the data to two and compute the error
+    # If not pure downsampling of the data at different composition ranges
+    if not downsampleData:
+        # If no threshold provided just do normal MLE
+        if not thresholdFlag:    
+            computedError = np.log(np.sum(np.power(moleFracExp - moleFracSim, 2)))
+            numPoints = len(moleFracExp)
+        # If threshold provided, split the data to two and compute the error
+        else:
+            #### Quite a lot of bugs because of changing how error computed ###
+            #### DO NOT USE THIS!!! ###
+            # Objective function error
+            # Find error for mole fraction below a given threshold
+            lastIndThreshold = int(np.argwhere(np.array(moleFracExp)>thresholdFactor)[-1])
+            # Do downsampling if the number of points in higher and lower
+            # compositions does not match
+            numPointsConc = np.zeros([2])
+            numPointsConc[0] = len(moleFracExp[0:lastIndThreshold]) # High composition
+            numPointsConc[1] = len(moleFracExp[lastIndThreshold:-1]) # Low composition            
+            downsampleConc = numPointsConc/np.min(numPointsConc) # Downsampled intervals
+            
+            # Compute error for higher concentrations (accounting for downsampling)
+            moleFracHighExp = moleFracExp[0:lastIndThreshold]
+            moleFracHighSim = moleFracSim[0:lastIndThreshold]
+            computedErrorHigh = np.log(np.sum(np.power(moleFracHighExp[::int(np.round(downsampleConc[0]))] 
+                                                        - moleFracHighSim[::int(np.round(downsampleConc[0]))],2)))
+            
+            # Find scaling factor for lower concentrations
+            scalingFactor = int(1/thresholdFactor) # Assumes max composition is one
+            # Compute error for lower concentrations
+            moleFracLowExp = moleFracExp[lastIndThreshold:-1]*scalingFactor
+            moleFracLowSim = moleFracSim[lastIndThreshold:-1]*scalingFactor
+        
+            # Compute error for low concentrations (accounting for downsampling)
+            computedErrorLow = np.log(np.sum(np.power(moleFracLowExp[::int(np.round(downsampleConc[1]))] 
+                                                        - moleFracLowSim[::int(np.round(downsampleConc[1]))],2)))
+            
+            # Find the sum of computed error
+            computedError = computedErrorHigh + computedErrorLow
+            
+            # Compute the number of points per experiment (accouting for down-
+            # sampling in both experiments and high and low compositions
+            numPoints = len(moleFracHighExp) + len(moleFracLowExp)
+    # Pure downsampling of the data at different composition ranges
     else:
         # Objective function error
+        # Concatenate the experiment and simulated data into an array
+        concatenatedData = np.vstack((moleFracExp,moleFracSim)).T
+        # Sort the data first (as everything is concatenated)
+        sortedData = concatenatedData[np.argsort(concatenatedData[:,0]),:]
         # Find error for mole fraction below a given threshold
-        lastIndThreshold = int(np.argwhere(np.array(moleFracExp)>thresholdFactor)[-1])
+        lastIndThreshold = int(np.argwhere(sortedData[:,0]>thresholdFactor)[0])
+        
         # Do downsampling if the number of points in higher and lower
         # compositions does not match
         numPointsConc = np.zeros([2])
-        numPointsConc[0] = len(moleFracExp[0:lastIndThreshold]) # High composition
-        numPointsConc[1] = len(moleFracExp[lastIndThreshold:-1]) # Low composition            
+        numPointsConc[0] = len(moleFracExp[0:lastIndThreshold]) # Low composition
+        numPointsConc[1] = len(moleFracExp[lastIndThreshold:-1]) # High composition            
         downsampleConc = numPointsConc/np.min(numPointsConc) # Downsampled intervals
         
-        # Compute error for higher concentrations (accounting for downsampling)
-        moleFracHighExp = moleFracExp[0:lastIndThreshold]
-        moleFracHighSim = moleFracSim[0:lastIndThreshold]
-        computedErrorHigh = np.log(np.sum(np.power(moleFracHighExp[::int(np.round(downsampleConc[0]))] 
-                                                    - moleFracHighSim[::int(np.round(downsampleConc[0]))],2)))
-        
-        # Find scaling factor for lower concentrations
-        scalingFactor = int(1/thresholdFactor) # Assumes max composition is one
-        # Compute error for lower concentrations
-        moleFracLowExp = moleFracExp[lastIndThreshold:-1]*scalingFactor
-        moleFracLowSim = moleFracSim[lastIndThreshold:-1]*scalingFactor
-    
-        # Compute error for low concentrations (accounting for downsampling)
-        computedErrorLow = np.log(np.sum(np.power(moleFracLowExp[::int(np.round(downsampleConc[1]))] 
-                                                    - moleFracLowSim[::int(np.round(downsampleConc[1]))],2)))
-        
-        # Find the sum of computed error
-        computedError = computedErrorHigh + computedErrorLow
+        # Compute error (accounting for downsampling)
+        # Lower concentrations
+        moleFracLowExp = moleFracExp[0:lastIndThreshold:int(np.round(downsampleConc[0]))]
+        moleFracLowSim = moleFracSim[0:lastIndThreshold:int(np.round(downsampleConc[0]))]
+        # Higher concentrations
+        moleFracHighExp = moleFracExp[lastIndThreshold:-1:int(np.round(downsampleConc[1]))]
+        moleFracHighSim = moleFracSim[lastIndThreshold:-1:int(np.round(downsampleConc[1]))]
+        # Compute the error
+        computedErrorLow = np.sum(np.power(moleFracLowExp - moleFracLowSim,2))
+        computedErrorHigh = np.sum(np.power(moleFracHighExp - moleFracHighSim,2))
+        computedError = np.log(computedErrorLow+computedErrorHigh)
         
         # Compute the number of points per experiment (accouting for down-
         # sampling in both experiments and high and low compositions

experimental/extractDeadVolume.py
@@ -17,6 +17,7 @@
 # Reference: 10.1007/s10450-012-9417-z
 #
 # Last modified:
+# - 2021-06-12, AK: Fix for error computation (major)
 # - 2021-05-28, AK: Add the existing DV model with MS DV model and structure change
 # - 2021-05-28, AK: Add model for MS
 # - 2021-05-24, AK: Improve information passing (for output)
@@ -96,16 +97,21 @@ def extractDeadVolume():
     if flagMSDeadVolume:
         msDeadVolumeFile = 'deadVolumeCharacteristics_20210528_1110_7c0209e.npz'
 
+    # Downsample the data at different compositions (this is done on 
+    # normalized data)
+    downsampleData = True
+
     # Threshold factor (If -negative infinity not used, if not need a float)
     # This is used to split the compositions into two distint regions
-    thresholdFactor = -np.inf
+    thresholdFactor = 0.5
 
     #####################################
     #####################################
 
     # Save the parameters to be used for fitting to a dummy file (to pass 
     # through GA - IDIOTIC)
-    savez ('tempFittingParametersDV.npz',thresholdFactor=thresholdFactor,
+    savez ('tempFittingParametersDV.npz', 
+           downsampleData=downsampleData,thresholdFactor=thresholdFactor,
            flagMSFit = flagMSFit, msFlowRate = msFlowRate,
            flagMSDeadVolume = flagMSDeadVolume, msDeadVolumeFile = msDeadVolumeFile)
     
@@ -173,6 +179,7 @@ def extractDeadVolume():
            msFlowRate = msFlowRate, # Flow rate through MS capillary [ccs]
            flagMSDeadVolume = flagMSDeadVolume, # Flag for checking if ms dead volume used
            msDeadVolumeFile = msDeadVolumeFile, # MS dead volume parameter file
+           downsampleFlag = downsampleData, # Flag for downsampling data [-]
            mleThreshold = thresholdFactor, # Threshold for MLE composition split [-]
            hostName = socket.gethostname()) # Hostname of the computer
 
@@ -197,6 +204,7 @@ def deadVolObjectiveFunction(x):
     
     # Load the threshold factor, MS fit flag and MS flow rate from the dummy 
     # file
+    downsampleData = load ('tempFittingParametersDV.npz')["downsampleData"]
     thresholdFactor = load ('tempFittingParametersDV.npz')["thresholdFactor"]
     flagMSFit = load ('tempFittingParametersDV.npz')["flagMSFit"] # Used only if MS data is fit
     msFlowRate = load ('tempFittingParametersDV.npz')["msFlowRate"] # Used only if MS data is fit
@@ -251,8 +259,10 @@ def deadVolObjectiveFunction(x):
         
         # Stack mole fraction from experiments and simulation for error 
         # computation
-        moleFracExpALL = np.hstack((moleFracExpALL, moleFracExp))
-        moleFracSimALL = np.hstack((moleFracSimALL, moleFracSim))
+        # Normalize the mole fraction by dividing it by maximum value to avoid
+        # irregular weightings for different experiment (at diff. scales)
+        moleFracExpALL = np.hstack((moleFracExpALL, (moleFracExp-np.min(moleFracExp))/(np.max(moleFracExp-np.min(moleFracExp)))))
+        moleFracSimALL = np.hstack((moleFracSimALL, (moleFracSim-np.min(moleFracSim))/(np.max(moleFracSim-np.min(moleFracSim)))))
         
     # Penalize if the total volume of the system is greater than experiemntal 
     # volume
@@ -261,7 +271,9 @@ def deadVolObjectiveFunction(x):
         penaltyObj = 10000
     # Compute the sum of the error for the difference between exp. and sim. and
     # add a penalty if needed (using MLE)
-    computedError = computeMLEError(moleFracExpALL,moleFracSimALL,thresholdFactor=thresholdFactor)
+    computedError = computeMLEError(moleFracExpALL,moleFracSimALL,
+                                    downsampleData=downsampleData,
+                                    thresholdFactor=thresholdFactor)
     return computedError + penaltyObj
 
 # func: filesToProcess

experimental/extractZLCParameters.py
@@ -16,6 +16,7 @@
 # Reference: 10.1016/j.ces.2014.12.062
 #
 # Last modified:
+# - 2021-06-12, AK: Fix for error computation (major)
 # - 2021-06-11, AK: Change normalization for error
 # - 2021-06-02, AK: Add normalization for error
 # - 2021-06-01, AK: Add temperature as an input
@@ -117,10 +118,15 @@ def extractZLCParameters():
     particleEpsilon = 0.61
     # Particle mass [g]
     massSorbent = 0.0625
-    
+
+    # Downsample the data at different compositions (this is done on 
+    # normalized data)
+    downsampleData = True
+        
     # Threshold factor (If -ngative infinity not used, if not need a float)
     # This is used to split the compositions into two distint regions
-    thresholdFactor = -np.inf
+    # Note that this is also needed for (pure) downsampling data
+    thresholdFactor = 0.5
     
     #####################################
     #####################################
@@ -167,7 +173,7 @@ def extractZLCParameters():
 
     # Initialize the parameters used for ZLC fitting process
     fittingParameters(True,temperature,deadVolumeFile,adsorbentDensity,particleEpsilon,
-                      massSorbent,isoRef,thresholdFactor,paramIso)
+                      massSorbent,isoRef,downsampleData,thresholdFactor,paramIso)
 
     # Algorithm parameters for GA
     algorithm_param = {'max_num_iteration':30,
@@ -220,6 +226,7 @@ def extractZLCParameters():
            particleEpsilon = particleEpsilon, # Particle voidage [-]
            massSorbent = massSorbent, # Mass of sorbent [g]
            parameterReference = isoRef, # Parameter references [-]
+           downsampleFlag = downsampleData, # Flag for downsampling data [-]
            mleThreshold = thresholdFactor, # Threshold for MLE composition split [-]
            hostName = socket.gethostname()) # Hostname of the computer
     
@@ -244,7 +251,7 @@ def ZLCObjectiveFunction(x):
     from computeMLEError import computeMLEError
 
     # Get the zlc parameters needed for the solver
-    temperature, deadVolumeFile, adsorbentDensity, particleEpsilon, massSorbent, isoRef, thresholdFactor, paramIso = fittingParameters(False,[],[],[],[],[],[],[],[])
+    temperature, deadVolumeFile, adsorbentDensity, particleEpsilon, massSorbent, isoRef, downsampleData, thresholdFactor, paramIso = fittingParameters(False,[],[],[],[],[],[],[],[],[])
 
     # Volume of sorbent material [m3]
     volSorbent = (massSorbent/1000)/adsorbentDensity
@@ -306,20 +313,22 @@ def ZLCObjectiveFunction(x):
         # computation
         # Normalize the mole fraction by dividing it by maximum value to avoid
         # irregular weightings for different experiment (at diff. scales)
-        # This might not be needed in DV model as all the experiments 
-        # start at 1 and end at 5e-3
-        moleFracExpALL = np.hstack((moleFracExpALL, (moleFracExp-np.min(moleFracExp))/np.max(moleFracExp))) 
-        moleFracSimALL = np.hstack((moleFracSimALL, (moleFracSim-np.min(moleFracSim))/np.max(moleFracSim)))
+        moleFracExpALL = np.hstack((moleFracExpALL, (moleFracExp-np.min(moleFracExp))/(np.max(moleFracExp-np.min(moleFracExp)))))
+        moleFracSimALL = np.hstack((moleFracSimALL, (moleFracSim-np.min(moleFracSim))/(np.max(moleFracSim-np.min(moleFracSim)))))
 
     # Compute the sum of the error for the difference between exp. and sim.
-    computedError = computeMLEError(moleFracExpALL,moleFracSimALL,thresholdFactor=thresholdFactor)
+    computedError = computeMLEError(moleFracExpALL,moleFracSimALL,
+                                    downsampleData=downsampleData,
+                                    thresholdFactor=thresholdFactor)
     return computedError
 
 # func: fittingParameters
 # Parses dead volume calibration file, adsorbent density, voidage, mass to 
 # be used for parameter estimation, parameter references and threshold for MLE
 # This is done because the ga cannot handle additional user inputs
-def fittingParameters(initFlag,temperature,deadVolumeFile,adsorbentDensity,particleEpsilon,massSorbent,isoRef, thresholdFactor,paramIso):
+def fittingParameters(initFlag,temperature,deadVolumeFile,adsorbentDensity,
+                      particleEpsilon,massSorbent,isoRef,downsampleData,
+                      thresholdFactor,paramIso):
     from numpy import savez
     from numpy import load
     # Process the data for python (if needed)
@@ -332,6 +341,7 @@ def fittingParameters(initFlag,temperature,deadVolumeFile,adsorbentDensity,parti
                particleEpsilon=particleEpsilon,
                massSorbent=massSorbent,
                isoRef=isoRef,
+               downsampleData=downsampleData,
                thresholdFactor=thresholdFactor,
                paramIso = paramIso)
     # Returns the path of the .npz file to be used 
@@ -345,6 +355,7 @@ def fittingParameters(initFlag,temperature,deadVolumeFile,adsorbentDensity,parti
         particleEpsilon = load (dummyFileName)["particleEpsilon"]
         massSorbent = load (dummyFileName)["massSorbent"]
         isoRef = load (dummyFileName)["isoRef"]
+        downsampleData = load (dummyFileName)["downsampleData"]
         thresholdFactor = load (dummyFileName)["thresholdFactor"]
         paramIso = load (dummyFileName)["paramIso"]
         return temperature, deadVolumeFile, adsorbentDensity, particleEpsilon, massSorbent, isoRef, thresholdFactor, paramIso